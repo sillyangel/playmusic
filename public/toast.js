252e71650d78c1edd2d488384467624d25ea1bfb
sillyangel <sillyangel3@gmail.com>
2023-10-29 08:21:47 -0500

    a

--- public/toast.js
+++
@@ -1,9 +0,0 @@
-function toast(a) {
-    var x = document.getElementById("snackbar");
-    x.className = "show";
-    x.innerHTML = text;
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-    return;
-}
-
-export { toast };
