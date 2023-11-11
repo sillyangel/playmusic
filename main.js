7afddaa9b1714c7482abd139d3d9f91acc1e5b3d
sillyangel <sillyangel3@gmail.com>
2023-11-11 13:01:34 -0600

    a

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-const port = 5502;
+const port = 80;
+
+app.use((req, res, next) => {
+    console.log(`User with IP: ${req.ip} made a request to ${req.originalUrl}`);
+    next();
+});
 
 app.use(express.static('public'))
-
-app.listen(port, () => {
-    console.log(`alive in port ${port}`)
-});
 
 app.use((req, res, next) => { 
     res.status(404).sendFile('./public/404.html', { root: __dirname });
 });
 
+app.listen(port, () => {
+    console.log(`alive in port ${port}`)
+});
