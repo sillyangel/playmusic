066d29a9b475d12f6584032882b3ff9044310925
aced8800 <aced8800@students.kleinisd.net>
2024-04-29 13:40:12 -0400

    Fix Auto_setup.sh to include my new tools needed

--- auto_setup.sh
+++ auto_setup.sh
@@ -1,15 +1,7 @@
 # Clone the repositories
 git clone git@gitlab.com:sillyangel/playmusic.git
 git clone git@gitlab.com:sillyangel/playcdn2.git
-
-# Change directory to playmusic
-cd playmusic
-
-# Install npm dependencies for playmusic
-npm install
-
-# Change directory back to the original directory
-cd ..
-
-# Install firebase-tools globally
-npm install -g firebase-tools
+# Install Firebase tools and yt-dlp standalone linux pkg
+curl -sL https://firebase.tools | bash
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux -o /usr/local/bin/yt-dlp
+chmod a+rx /usr/local/bin/yt-dlp
