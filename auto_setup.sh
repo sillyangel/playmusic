49fac6f5a829fef59d97b191b3cffdf9b8963961
aced8800 <aced8800@students.kleinisd.net>
2024-04-29 15:13:23 -0400

    fix auto_setup

--- auto_setup.sh
+++ auto_setup.sh
@@ -1,7 +1,8 @@
-# Clone the repositories
+# Clone the repositories needed for development
 git clone git@gitlab.com:sillyangel/playmusic.git
 git clone git@gitlab.com:sillyangel/playcdn2.git
 # Install Firebase tools and yt-dlp standalone linux pkg
 curl -sL https://firebase.tools | bash
 curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux -o /usr/local/bin/yt-dlp
+# chmod new yt-dlp
 chmod a+rx /usr/local/bin/yt-dlp
