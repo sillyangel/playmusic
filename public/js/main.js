e84043712c7f23162b411e441d53473e418058d7
angel <sillyangel3@gmail.com>
2024-05-02 23:22:42 +0000

    fix

--- public/js/main.js
+++ public/js/main.js
@@ -1582,7 +1582,6 @@ function searchfunction() {
             tracklist.appendChild(p);
         }
     }   
-    albumpage(41)
     function darkmode() {
         var element = document.body;
         element.classList.toggle("dark-mode");
